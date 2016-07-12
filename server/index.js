034f6d37bda3e2c796fd869b9b5576a665cbd78e
Matt Long <mlong635@gmail.com>
2016-07-12 12:55:16 -0700

    trying to update files

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -12,16 +11,11 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res){
  res.sendFile(__dirname +'/../client/index.html');
+
 });
 
 
-
 io.on('connection', function(socket){
-
-  socket.on('playNpause', function(cb){
-    console.log('SERVER CAUGHT', cb);
-    socket.broadcast.emit('playNpause', cb);
-  });
 
   socket.on('playNpause', function(cb){
     console.log('SERVER CAUGHT', cb);
@@ -29,26 +23,30 @@ io.on('connection', function(socket){
   });
 
   socket.on('username', function(name){
+    console.log(name);
+    // if(addedUser){return; };
     socket['name'] = name;
-    
+    // addedUser = true;
+
+    // socket.broadcast.emit('user joined', {
+    //   username: socket.name
+    // })
   });
 
   socket.on('chat message', function(msg){
     var obj = {};
     obj['message'] = msg;
-    
-
     io.emit('chat message', socket['name'] + ": " + msg);
     console.log('message: ' + msg);
   });
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function () {
+    // socket.broadcast.emit('typing', {
     socket.broadcast.emit('typing', {
       name: socket.name
     });
   });
-
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', function () {
     socket.broadcast.emit('stop typing', {
@@ -59,29 +57,10 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-
-  // when the client emits 'startPlay', we broadcast it to others
-  socket.on('startPlay', function(){
-    console.log("index.js socket.on startPlay invoked");
-    socket.broadcast.emit('startPlay', {
-      name: socket.name
-    });
-  });
-
-  // when the client emits 'stopPlay', we broadcast it to others
-  socket.on('stopPlay', function(){
-    console.log("index.js socket.on stopPlay invoked");
-    socket.broadcast.emit('stopPlay', {
-      name: socket.name
-    });
-  });
-
 });
-
 
 //Initializing http on io makes it so its listening on this port
 http.listen(process.env.PORT || 8000, function(){
   console.log('App listening on port 8000');
 });
-
 
